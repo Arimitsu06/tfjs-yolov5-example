7804ddb8064da3732203867eb9d5597faae27778
Ari <arimitsu0803@gmail.com>
2022-03-26 11:51:31 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/tfjs-yolov5-example/precache-manifest.446f2562a99fe4f1a0c719bd2521698f.js"
+  "/tfjs-yolov5-example/precache-manifest.76fec74c62a6b289f445363c7e8a77a3.js"
 );
 
 workbox.clientsClaim();
