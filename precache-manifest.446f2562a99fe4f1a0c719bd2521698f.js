88b12ca003759db9165a0775668014ac8bf8962b
Ari <arimitsu0803@gmail.com>
2022-03-26 11:28:06 -0400

    Updates

---
+++ precache-manifest.446f2562a99fe4f1a0c719bd2521698f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d686f760dfaf5cb8372f",
+    "url": "/tfjs-yolov5-example/static/js/runtime~main.d686f760.js"
+  },
+  {
+    "revision": "82caf5b78b292739470b",
+    "url": "/tfjs-yolov5-example/static/js/main.82caf5b7.chunk.js"
+  },
+  {
+    "revision": "9e96ffa140403a605beb",
+    "url": "/tfjs-yolov5-example/static/js/1.9e96ffa1.chunk.js"
+  },
+  {
+    "revision": "82caf5b78b292739470b",
+    "url": "/tfjs-yolov5-example/static/css/main.ac9d7858.chunk.css"
+  },
+  {
+    "revision": "0e7582bfedb5e5dc1905a36f1ed908e3",
+    "url": "/tfjs-yolov5-example/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/tfjs-yolov5-example/precache-manifest.4da455c031fcd4c97179b6903eec0b72.js"
+  "/tfjs-yolov5-example/precache-manifest.446f2562a99fe4f1a0c719bd2521698f.js"
 );
 
 workbox.clientsClaim();
